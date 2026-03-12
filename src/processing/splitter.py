2a873466ed18333aa5d47ca83abae0314dd421b8
AyushAI <ayush.wase.ai@ghrce.raisoni.net>
2026-03-12 11:40:31 +0530

    Added : Recursive Character Text Splitter

---
+++ src/processing/splitter.py
@@ -0,0 +1,12 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def split_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 800,
+        chunk_overlap = 150
+    )
+    
+    chunks = splitter.split_documents(documents)
+    print(f"Created {len(chunks)} chunks")
+    
+    return chunks
